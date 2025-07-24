fc5ed5c0ffe1015f41e3cfbffcb7ba0e3303264f
Fefemyster <kenennlara@gmail.com>
2025-07-23 19:55:52 -0600

    Clase 1 Semana 1

--- Pedidos/Program.cs
+++ Pedidos/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Pedidos;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Producto producto = new Producto();
+producto.Id = 1;
+producto.Nombre = "Laptop";
+producto.Precio = 5000;
+producto.Stock = 10;
+
+producto.MostrarInformacion();
+producto.DisminuirStock(1);
+producto.MostrarInformacion();
+producto.MostrarInformacion();
+producto.DisminuirStock(15);
+producto.MostrarInformacion();
+
+
+Producto producto1= new Producto();
+producto.Id = 2;
+producto.Nombre = "Mouse";
+producto.Precio = 250;
+producto.Stock = 15;
+
+producto1.MostrarInformacion();
+producto1.DisminuirStock(1);
+producto1.MostrarInformacion();
+producto1.MostrarInformacion();
+producto1.DisminuirStock(15);
+producto1.MostrarInformacion();
